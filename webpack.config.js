ea179e405878eb3b5236837da0f26eab12846771
sun2545 <kwonsunhye0206@gmail.com>
2018-08-31 20:04:50 -0400

    add babel

---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+var path = require('path');
+
+module.exports = {
+    entry: './src/index.js',
+    output :{
+        filename :'bundle.js',
+        path:path.resolve(__dirname,'dist')
+    },
+    module:{
+        rules :[{
+            test: /\.js$/,
+            include: path.resolve(__dirname,'src'),
+            use:{
+                loader : 'babel-loader',
+                options : {
+                    presets : ['env', {
+                        'browsers' : ["last 2 versions"]
+                    }]
+                }
+            }
+
+        }]
+    }
+}
