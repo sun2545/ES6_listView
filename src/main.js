7cb2b5a08f5691b3d9b8728430628dc7e08c8a76
sun2545 <kwonsunhye0206@gmail.com>
2018-09-02 18:43:08 -0400

    XHR communication

--- src/main.js
+++ src/main.js
@@ -1,6 +1,21 @@
 class Blog {
     constructor(){
-        console.log('Blog is started');
+       const dataUGL="https://";
+        this.setInitData(dataURL);
+    }
+    setInitData(dataURL){
+        this.getData(dataURL);
+    }
+    getData(dataURL){
+        const oReq = new XMLHttpRequest();
+        oReq.addEventListener("load", () => {
+            const list =JSON.parse(JSON.parse(oReq.responseText).body);
+          //  console.log('list is ', JSON.parse(list.body));
+
+            
+        });
+        oReq.open('GET', dataURL);
+        oReq.send();
     }
 }
 
