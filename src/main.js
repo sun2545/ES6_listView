fc766ea7f544d9376034c1c6d9ad636adf5af310
sun2545 <kwonsunhye0206@gmail.com>
2018-09-03 00:24:02 -0400

    add XHR and list

--- src/main.js
+++ src/main.js
@@ -1,21 +1,29 @@
 class Blog {
     constructor(){
-       const dataUGL="https://";
+       const dataUGL=" https://github.com/nigayo/inflearn-es6/tree/step4";
         this.setInitData(dataURL);
     }
     setInitData(dataURL){
-        this.getData(dataURL);
+        this.getData(dataURL, this.insertPost);
     }
-    getData(dataURL){
+
+    getData(dataURL, fn){
         const oReq = new XMLHttpRequest();
         oReq.addEventListener("load", () => {
             const list =JSON.parse(JSON.parse(oReq.responseText).body);
           //  console.log('list is ', JSON.parse(list.body));
-
+         fn(list);
             
         });
         oReq.open('GET', dataURL);
         oReq.send();
+    }
+
+    insertPost(list){
+        const ul = document.querySelector(".blogList > ul");
+        list.forEach((v) =>{
+            ul.innerHTML += `<li><a href = ${v.link}> ${v.title} </a></li>`;
+        })
     }
 }
 
